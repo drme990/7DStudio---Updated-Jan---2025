2b32ba217c9d8384b79799e90d31cafcc92960d0
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2025-01-26 17:37:35 +0200

    (fix): trying meta pixel

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { initializeMetaPixel } from './services/MetaPixel.js';
 import App from './App.jsx';
 import './index.css';
-
-// Initialize Meta Pixel
-initializeMetaPixel();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'process';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_PIXEL_ID': JSON.stringify(
+      process.env.NEXT_PUBLIC_PIXEL_ID
+    ),
+  },
+});
